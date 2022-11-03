55589680f6147f65ae6cd08b5f45a57a3483afd1
joshkat <joshua02k@gmail.com>
2022-11-03 17:27:52 -0400

    init rand arr to use in representation

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-//swap helper function
-function swap(nums, a, b){
-    var temp = nums[a];
-    nums[a] = nums[b];
-    nums[b] = temp;
-}
 
-function bubbleSort(nums){
-    for(var i=0; i<nums.length; i++){
-        for(var j=0; j< (nums.length-i-1); j++){
-            if(nums[j] > nums[j+1]){
-                var temp = nums[j]
-                nums[j] = nums[j+1]
-                nums[j+1] = temp
-            }
-        } 
+//create random array
+var randomArray = []
+function initArray(arr){    //using func so i can have reset btn later on
+    for(i=0; i<20; i++){
+        arr[i] = Math.floor(Math.random() * 100) + 1 //randomnum 1-100
     }
-    console.log(nums)
+    console.log(arr)
 }
+initArray(randomArray)
 
-var testingArray = [12, 3, 4, 5, 3, 2, 1, 43]
-bubbleSort(testingArray)
