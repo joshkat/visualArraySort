1e632d2987b40fe32482b943dd0fdbe37da725fb
joshkat <joshua02k@gmail.com>
2022-11-03 23:22:14 -0400

    adjusted spacing, added bblsrt w/o timer

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const arrayBox = document.getElementById('arrayContainer')
+const sortBtn = document.getElementById('startAlgo')
+const resetBtn = document.getElementById('reset')
 
 //create random array
 var randomArray = []
 function initArray(arr){    //using func so i can have reset btn later on
-    for(i=0; i<20; i++){
-        arr[i] = Math.floor(Math.random() * 100) + 1 //randomnum 1-100
+    for(i=0; i<30; i++){
+        arr[i] = Math.floor(Math.random() * 200) + 1 //randomnum 1-200
     }
-    console.log(arr)
 }
 initArray(randomArray)
 
@@ -15,9 +16,43 @@ function createBoxes(arr){
     for(i=0; i<arr.length; i++){
         var bar = document.createElement('div')
         bar.style.height = arr[i] + 'px'
-        bar.classList.add(i)
+        bar.classList.add('bar')
+        // bar.innerText = arr[i]
         arrayBox.appendChild(bar)
     }
 }
-
 createBoxes(randomArray)
+
+sortBtn.addEventListener('click', ()=>{
+    bubbleSort(randomArray, arrayBox)
+})
+
+resetBtn.addEventListener('click', ()=>{
+    clearBoard(arrayBox)
+    initArray(randomArray)
+    createBoxes(randomArray)
+})
+
+//sort functions down here
+function bubbleSort(nums, board){
+    for(var i=0; i<nums.length; i++){
+        for(var j=0; j< (nums.length-i-1); j++){
+            if(nums[j] > nums[j+1]){
+                var temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+                //this is where we get a little bit risky :smirk:
+                clearBoard(board)                    
+                createBoxes(nums)
+            }
+        }
+    }  
+}
+
+//helper function for sort
+function clearBoard(board){
+    while(board.firstChild){
+        board.removeChild(board.firstChild);
+    }
+}
+
