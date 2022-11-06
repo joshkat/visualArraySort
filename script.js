de51be2cdc3e6cbc37016b3f8a38d0de4e718207
joshkat <joshua02k@gmail.com>
2022-11-06 13:29:52 -0500

    fixed btns, added selection sort

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const arrayBox = document.getElementById('arrayContainer')
 const sortBtn = document.getElementById('startAlgo')
 const resetBtn = document.getElementById('reset')
 const bubbleBtn = document.getElementById('bubbleSort')
+const selectionBtn = document.getElementById('selectionSort')
 const mergeBtn = document.getElementById('mergeSort')
 const heapBtn = document.getElementById('heapSort')
 const quickBtn = document.getElementById('quickSort')
@@ -36,6 +37,32 @@ createBoxes(randomArray)
 
 //header control btns
 
+bubbleBtn.addEventListener('click', ()=>{
+    currentAlgo = 'bubbleSort'
+})
+
+selectionBtn.addEventListener('click', ()=>{
+    currentAlgo = 'selectionSort'
+})
+
+mergeBtn.addEventListener('click', ()=>{
+    currentAlgo = 'mergeSort'
+})
+
+heapBtn.addEventListener('click', ()=>{
+    currentAlgo = 'heapSort'
+})
+
+quickBtn.addEventListener('click', ()=>{
+    currentAlgo = 'quickSort'
+})
+
+radixBtn.addEventListener('click', ()=>{
+    currentAlgo = 'radixSort'
+    console.log(currentAlgo)
+})
+
+
 sortBtn.addEventListener('click', ()=>{
     currentlySorting = true
     resetBtn.classList.add('redButton')
@@ -68,7 +95,23 @@ async function bubbleSort(nums, board){
     currentlySorting = false  
 }
 
-//helper functions for sort
+async function selectionSort(nums, board){
+    for (var i = 0; i < nums.length-1; i++){
+        var min_idx = i;
+        for (var j = i + 1; j < nums.length; j++)
+        if (nums[j] < nums[min_idx])
+            min_idx = j;
+ 
+        swap(nums,min_idx, i);
+        clearBoard(board)
+        createBoxes(nums)
+        await sleep(100)
+    }
+    resetBtn.classList.remove('redButton')
+    currentlySorting = false  
+}
+
+//helper functions for sort algos
 function clearBoard(board){
     while(board.firstChild){
         board.removeChild(board.firstChild)
@@ -79,10 +122,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+function swap(arr, xp, yp){
+    var temp = arr[xp];
+    arr[xp] = arr[yp];
+    arr[yp] = temp;
+}
+
 function preformCurrentAlgo(algoName){
     switch(algoName){
         case 'bubbleSort':
             bubbleSort(randomArray, arrayBox)
+            break
+        case 'selectionSort':
+            selectionSort(randomArray,arrayBox)
             break
         case 'mergeSort':
             mergeSort(randomArray, arrayBox)
@@ -94,7 +146,7 @@ function preformCurrentAlgo(algoName){
             quickSort(randomArray, arrayBox)
             break
         case 'radixSort':
-            quickSort(randomArray, arrayBox)
+            radixSort(randomArray, arrayBox)
             break
     }
 }
